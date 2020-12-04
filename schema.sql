a3c4907b82dbe9cba31150d4089573b38898cf4e
lrios215 <70179261+lrios215@users.noreply.github.com>
2020-12-03 19:42:31 -0500

    schema_sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE happiness (
+country_name TEXT,
+happiness_rank INT PRIMARY KEY,
+happiness_score INT
+);
+
+CREATE TABLE country_code (
+country_code INT PRIMARY KEY,
+country_name TEXT,
+);
+
+CREATE TABLE country_info (
+country_name TEXT PRIMARY KEY,
+population INT,
+fertility INT,
+life_expectacy INT,
+GDP INT
+);
+
+
+-- Joins tables
+SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
+FROM customer_name
+JOIN customer_location
+ON customer_name.id = customer_location.id;
